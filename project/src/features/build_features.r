723e7bd234b0536bd1316fdf316ad2fe24ac86e7
Zinuo Jia <jiazinuo3@gmail.com>
2020-02-03 13:59:54 -0500

    updata on template

--- project/src/features/build_features.r
+++ project/src/features/build_features.r
@@ -1,1 +1,10 @@
 library("data.table")
+library(DataComputing)
+library(Metrics)
+
+readDir = "../../volume/data/raw"
+writeDir = "../../volume/data/processed"
+exterDir = "../../volume/data/external"
+interDir = "../../volume/data/interim"
+
+
